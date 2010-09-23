848ee89f1cb2199d1cde2d28fbc6fae7cf1dfff7
Brian Hicks <therealcaffeine@gmail.com>
2010-09-23 11:08:27 -0500

    added sign in and out links

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ SPScaffold::Application.routes.draw do
   match "about" => "pages#about"
   match "contact" => "pages#contact"
 
-  resources :surveys
+  resources :surveys, :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
