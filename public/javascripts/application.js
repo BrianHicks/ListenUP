d4f6c421d82e2e049a68fa309291002fbfa8e382
Brian Hicks <therealcaffeine@gmail.com>
2010-09-22 22:36:03 -0500

    adding and removing questions and answers works now.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,8 +6,14 @@ function remove_fields(link) {
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).previous().insert({
-    before: content.replace(regexp, new_id)
-  });
+  if (association == "answers") {
+    $(link).previous().insert({
+      before: content.replace(regexp, new_id)
+    });
+  } else {
+    $(link).up().previous().insert({
+      before: content.replace(regexp, new_id)
+    })
+  }
 }
 
