31479ea2a3fd6e19c9b213e191416b132968159d
Brian Hicks <therealcaffeine@gmail.com>
2010-09-22 20:24:20 -0500

    remove and add answer links work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,12 @@
 function remove_fields(link) {
   $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
+  $(link).up(".inputs").hide();
 }
 
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
+  $(link).previous().insert({
     before: content.replace(regexp, new_id)
   });
 }
