089d578059fc250164e8bd1042473d7b2f59dde9
Brian Hicks <therealcaffeine@gmail.com>
2010-09-23 10:41:37 -0500

    added devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SPScaffold::Application.routes.draw do
 
+  devise_for :users
+
   root :to => "pages#home"
 
   match "help" => "pages#help"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100922060228) do
+ActiveRecord::Schema.define(:version => 20100923154014) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -38,5 +38,28 @@ ActiveRecord::Schema.define(:version => 20100922060228) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.boolean  "is_admin"
+    t.string   "pub_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
