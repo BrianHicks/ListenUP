75a7678ab6ba2022895f922ce9cf56d70ecc8c4c
Brian Hicks <therealcaffeine@gmail.com>
2010-09-24 00:30:47 -0500

    fixed order

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   
-  has_many :answers, :dependent => :destroy, :order => "order"
+  has_many :answers, :dependent => :destroy
   
   accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? },
                                           :allow_destroy => true
