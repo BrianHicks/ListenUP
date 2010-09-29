e06f8fc95b46f51d500c1691afee2a46a563eef7
Brian Hicks <therealcaffeine@gmail.com>
2010-09-28 21:38:31 -0500

    added blueprints

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,4 +1,5 @@
 require 'machinist/active_record'
+require 'faker'
 
 # Add your blueprints here.
 #
@@ -7,3 +8,37 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+User.blueprint do
+  email { Faker::Internet.email }
+  password { "password" }
+  password_confirmation { "password" }
+  is_admin { false }
+end
+
+User.blueprint(:admin) do
+  is_admin { true }
+end
+
+Survey.blueprint do
+  name { Faker::Lorem.words(2) }
+  intro { Faker::Lorem.words(40) }
+  thank_you { Faker::Lorem.words(40) }
+end
+
+Question.blueprint do
+  content { Faker::Lorem.words(8) }
+  kind { "text" }
+end
+
+Recipient.blueprint do
+  email { Faker::Internet.email }
+end
+
+Recipient.blueprint(:no_email) do
+  no_email { true }
+end
+
+Answer.blueprint do
+  content { Faker::Lorem.words(3) }
+end
