86cbfe6e5ac2f2b44ebf66816f808ee72496fe7d
Brian Hicks <therealcaffeine@gmail.com>
2010-10-06 15:36:50 -0500

    questions now have correct sort order in order field.

--- public/javascripts/survey_edit.js
+++ public/javascripts/survey_edit.js
@@ -2,7 +2,15 @@ $(document).ready(function() {
   // make the questions sortable
   $("#questions").sortable({
     containment: jQuery("#questions"),
-    axis: "y"
+    axis: "y",
+    update: function() {
+      // update orders by iterating through order fields
+      var i = 0;
+      $(".order").each(function() {
+        $(this).val(i);
+        i++;
+      });
+    }
   });
   // make the order fields hidden
   jQuery(".order").parent().hide()
