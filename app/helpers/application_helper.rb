2cf0aeca1a47ecaf19ebee010cf8628f6be4df7c
Brian Hicks <therealcaffeine@gmail.com>
2010-10-07 17:02:29 -0500

    adding everything else works.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   def link_to_add_fields(name, f, association)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder)
+      "<fieldset class=\"#{association.to_s.singularize}\"><ol>#{render(association.to_s.singularize + "_fields", :f => builder)}</ol></fieldset>"
     end
     coder = HTMLEntities.new
     link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
--- public/javascripts/survey_edit.js
+++ public/javascripts/survey_edit.js
@@ -1,21 +1,22 @@
 $(document).ready(function() {
   // make the questions sortable
   $("#questions").sortable({
-    axis: "y",
-    update: function() {
-      // update orders by iterating through order fields
-      var i = 0;
-      $(".order").each(function() {
-        $(this).val(i);
-        i++;
-      });
-    }
+    axis: "y"
+  });
+});
+
+var updateInfo = function() {
+  // update orders by iterating through order fields
+  var i = 0;
+  $(".order").each(function() {
+    $(this).val(i);
+    i++;
   });
   // make the order fields hidden
   $(".order").parent().hide();
   // make the answers hidden by default
   $(".answers").hide();
-});
+}
 
 var toggleVisible = function(selector) {
   $(selector).toggle();
@@ -32,5 +33,17 @@ var remove_field = function(selector) {
     //$(selector).animate({ backgroundColor: "#A00", "color": "#FFF"}, 500)
     $(".destroy", selector).val("1");
     $("input, select", selector).attr("disabled", "true");
+  }
+}
+
+var add_fields = function(sender, selector, html) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + selector, "g")
+  html = html.replace(regexp, new_id);
+  if (selector == "answers") {
+    // append to the parent question's answers
+    $(".answers", $(sender).parents(".question")).append(html);
+  } else {
+    $("#" + selector).append(html);
   }
 }
