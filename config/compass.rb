08d7a14488a2c9d57af1e5bce0f473d4da5f36c6
Brian Hicks <therealcaffeine@gmail.com>
2010-11-03 00:25:07 -0500

    stylesheets!

--- config/compass.rb
+++ config/compass.rb
@@ -2,10 +2,12 @@
 # Require any additional compass plugins here.
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
+require 'lemonade'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
 sass_dir = "app/stylesheets"
+images_dir = "public/images/"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
