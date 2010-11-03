0507a35f2390bb06571f1b7b51c55f5415a6cf0f
Brian Hicks <therealcaffeine@gmail.com>
2010-11-03 00:48:58 -0500

    conditionallly added lemonade

--- config/compass.rb
+++ config/compass.rb
@@ -2,7 +2,7 @@
 # Require any additional compass plugins here.
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
-require 'lemonade'
+require 'lemonade' if Rails.env.development?
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets/compiled"
