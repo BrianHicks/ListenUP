184f138461bc4bc5ef998e7c14dfd957bc6c55ef
Brian Hicks <therealcaffeine@gmail.com>
2010-11-30 22:35:13 -0600

    removed RSS fetching. So THATs why the home page was so slow!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,6 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
-    @news = "<h1>News and Updates</h1>"
-    
-    feed_url = "http://listenupapp.tumblr.com/rss"
-    open (feed_url) do |http|
-      response = http.read
-      result = RSS::Parser.parse(response, false)
-      result.channel.items.each do |item|
-        @news += "<h2>#{item.title}</h2>#{item.description}<p>#{item.pubDate.to_date}</p>"
-      end
-    end
   end
 
   def help
